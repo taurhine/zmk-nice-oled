10298779ce7ac327ea028f51d4ba873c7b50c371
rive7 <rive7@rive7.de>
2026-01-27 01:45:29 +0100

    fixing 4 frame anim limit

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -36,7 +36,7 @@ LV_IMG_DECLARE(head_03);
 LV_IMG_DECLARE(head_04);
 LV_IMG_DECLARE(head_05);
 
-static const lv_img_dsc_t * const head_imgs[] = {&head_00, &head_01, &head_02, &head_03, &head_04, &head_05};
+static const lv_img_dsc_t * const head_imgs[6] = {&head_00, &head_01, &head_02, &head_03, &head_04, &head_05};
 
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_CAT)
 LV_IMG_DECLARE(cat_0);
