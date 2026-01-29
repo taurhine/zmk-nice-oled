989dcee00f795b71d256090a419e17af6ec7624c
rive7 <rive7@rive7.de>
2026-01-29 18:35:11 +0100

    Improved the animation

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -35,9 +35,16 @@ LV_IMG_DECLARE(head_02);
 LV_IMG_DECLARE(head_03);
 LV_IMG_DECLARE(head_04);
 LV_IMG_DECLARE(head_05);
+LV_IMG_DECLARE(head_06);
+LV_IMG_DECLARE(head_07);
+LV_IMG_DECLARE(head_08);
+LV_IMG_DECLARE(head_09);
+LV_IMG_DECLARE(head_10);
+LV_IMG_DECLARE(head_11);
 
 static const lv_img_dsc_t * const head_imgs[] = {
-    &head_00, &head_01, &head_02, &head_03, &head_04, &head_05
+    &head_00, &head_01, &head_02, &head_03, &head_04, &head_05,
+    &head_06, &head_07, &head_08, &head_09, &head_10, &head_11,
 };
 
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_CAT)
@@ -174,7 +181,7 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
         lv_obj_center(art);
 
 #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_HEAD)
-        lv_animimg_set_src(art, (const void **)head_imgs, 6);
+        lv_animimg_set_src(art, (const void **)head_imgs, 12);
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_CAT)
         lv_animimg_set_src(art, (const void **)cat_imgs, 8);
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_SPACEMAN)
