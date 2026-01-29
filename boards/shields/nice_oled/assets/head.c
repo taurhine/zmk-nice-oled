feecc945c3c969bac2ee2a0c5d68da498dc51258
rive7 <rive7@rive7.de>
2026-01-29 18:55:39 +0100

    brought the const back to animation.c

--- boards/shields/nice_oled/assets/head.c
+++ boards/shields/nice_oled/assets/head.c
@@ -929,8 +929,3 @@ LV_IMG_DECLARE(head_08);
 LV_IMG_DECLARE(head_09);
 LV_IMG_DECLARE(head_10);
 LV_IMG_DECLARE(head_11);
-
-const lv_img_dsc_t * const head_imgs[] = {
-    &head_00, &head_01, &head_02, &head_03, &head_04, &head_05,
-    &head_06, &head_07, &head_08, &head_09, &head_10, &head_11,
-};
--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -29,6 +29,11 @@ const lv_img_dsc_t *crystal_imgs[] = {
 #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL)
 #if IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_HEAD)
 
+const lv_img_dsc_t * const head_imgs[] = {
+    &head_00, &head_01, &head_02, &head_03, &head_04, &head_05,
+    &head_06, &head_07, &head_08, &head_09, &head_10, &head_11,
+};
+
 #elif IS_ENABLED(CONFIG_NICE_OLED_WIDGET_ANIMATION_PERIPHERAL_CAT)
 LV_IMG_DECLARE(cat_0);
 LV_IMG_DECLARE(cat_1);
